5b9002c728b6a4085e702aea09a10a9c05d24c58
arruchetz <daniel.gararr@educa.jcyl.es>
2023-02-06 14:55:13 +0100

    Pizzeria

--- src/com/danielgararr/pizzeria/presentation/Main.java
+++ src/com/danielgararr/pizzeria/presentation/Main.java
@@ -1,36 +1,40 @@
 package com.danielgararr.pizzeria.presentation;
 
-import com.danielgararr.pizzeria.domain.models.Barbacoa;
-import com.danielgararr.pizzeria.domain.models.Campera;
-import com.danielgararr.pizzeria.domain.models.Carbonara;
-import com.danielgararr.pizzeria.domain.models.Pizza;
+import com.danielgararr.pizzeria.domain.models.*;
 
 public class Main {
     public static void main(String[] args) {
 
         //Crear Pizza Barbacoa
         Barbacoa barbacoa = new Barbacoa();
-        barbacoa.getId(1);
-        barbacoa.getNombre("Pizza Barbacoa");
-        barbacoa.getSalsa("Barbacoa");
-        barbacoa.getExtras("Pollo");
-        barbacoa.getPicante(true);
+        barbacoa.setId(1);
+        barbacoa.setNombre("Pizza Barbacoa");
+        barbacoa.setSalsa("Barbacoa");
+        barbacoa.setExtras("Pollo");
+        barbacoa.setPicante(true);
 
         //Crear Pizza Carbonara
         Carbonara carbonara = new Carbonara();
-        carbonara.getId(2);
-        carbonara.getNombre("Pizza Carbonara");
-        carbonara.getSalsa("Queso");
-        carbonara.getExtras("Cebolla");
-        carbonara.getQuesoAzul(true);
-        carbonara.getQuesoCabra(false);
+        carbonara.setId(2);
+        carbonara.setNombre("Pizza Carbonara");
+        carbonara.setSalsa("Queso");
+        carbonara.setExtras("Cebolla");
+        carbonara.setQuesoAzul(true);
+        carbonara.setQuesoCabra(false);
 
         //Crear Pizza Campera
         Campera campera = new Campera();
-        campera.getId(3);
-        campera.getNombre("Pizza Campera");
-        campera.getSalsa("Tomate");
-        campera.getExtras("Aceituna");
-        campera.getCarne(true);
+        campera.setId(3);
+        campera.setNombre("Pizza Campera");
+        campera.setSalsa("Tomate");
+        campera.setExtras("Aceituna");
+        campera.setCarne(true);
+
+        //Crear Horno
+        Horno horno = new Horno();
+        horno.setId(1);
+        horno.setNombre("Horno A");
+        horno.setMarca("Philips");
+        horno.setTemperatura("200 Grados");
     }
 }
