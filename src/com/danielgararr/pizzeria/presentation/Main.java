6e429e25817e7d95ac8f54aef1d2b86e708c2e66
arruchetz <daniel.gararr@educa.jcyl.es>
2023-02-06 14:30:51 +0100

    Pizzeria

--- src/com/danielgararr/pizzeria/presentation/Main.java
+++ src/com/danielgararr/pizzeria/presentation/Main.java
@@ -1,7 +1,36 @@
 package com.danielgararr.pizzeria.presentation;
 
+import com.danielgararr.pizzeria.domain.models.Barbacoa;
+import com.danielgararr.pizzeria.domain.models.Campera;
+import com.danielgararr.pizzeria.domain.models.Carbonara;
+import com.danielgararr.pizzeria.domain.models.Pizza;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Crear Pizza Barbacoa
+        Barbacoa barbacoa = new Barbacoa();
+        barbacoa.getId(1);
+        barbacoa.getNombre("Pizza Barbacoa");
+        barbacoa.getSalsa("Barbacoa");
+        barbacoa.getExtras("Pollo");
+        barbacoa.getPicante(true);
+
+        //Crear Pizza Carbonara
+        Carbonara carbonara = new Carbonara();
+        carbonara.getId(2);
+        carbonara.getNombre("Pizza Carbonara");
+        carbonara.getSalsa("Queso");
+        carbonara.getExtras("Cebolla");
+        carbonara.getQuesoAzul(true);
+        carbonara.getQuesoCabra(false);
+
+        //Crear Pizza Campera
+        Campera campera = new Campera();
+        campera.getId(3);
+        campera.getNombre("Pizza Campera");
+        campera.getSalsa("Tomate");
+        campera.getExtras("Aceituna");
+        campera.getCarne(true);
     }
 }
